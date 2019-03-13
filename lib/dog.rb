e7a9174b650d3c24778a257f5eb4ea8c2d0e0830
authentic-actor-8801 <xaviernational@aol.com>
2019-03-13 17:03:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,7 @@
 class Dog
 
-<<<<<<< HEAD
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-=======
-  def initialize(name)
-    @name = name
-    @breed = "Mutt"
->>>>>>> 68c7c91aaa68fd723c3da241ad35d4c0835b75bb
   end
 end
